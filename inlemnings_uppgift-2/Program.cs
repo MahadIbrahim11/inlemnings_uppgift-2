35baf3ce248c5f49ebcb1f4153ec688c019d65c1
maib07001 <maib07001@E-PW05C37V>
2024-10-10 14:24:15 +0200

    Add project files.

---
+++ inlemnings_uppgift-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace inl_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Memery slot \"3\" has ben corupded");
+
+
+        }
+    }
+}
